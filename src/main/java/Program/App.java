a44195ae55a658226f60645e9ea81621136acc1b
Alexander Falk <alexanderfalk@Alexanders-MacBook-Pro.local>
2017-10-20 13:10:41 +0200

    Added RabbitMQ to publish a message to Group14_Queue

--- src/main/java/Program/App.java
+++ src/main/java/Program/App.java
@@ -1,6 +1,10 @@
 package Program;
 
 import Settings.BankJSON;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.json.JSONObject;
 
 import javax.json.Json;
@@ -13,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 @Path("/bank")
 public class App {
@@ -55,8 +60,36 @@ public class App {
                     .add("interestRate", temp.get(1).toString())
                     .add("refund", temp.get(2).toString()))
             .build();
-
+        publishQueue(value.toString());
         return Response.ok().entity(value.toString()).build();
+    }
+
+    public static void publishQueue(String message) {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("5.179.80.218");
+            Connection connection = factory.newConnection();
+
+            Channel replyChannel = connection.createChannel();
+            String replyQueue = "Group14_Bank_Response_Queue";
+            replyChannel.queueDeclare(replyQueue , false, false, false, null);
+            String replyKey = "Group14_Bank_Response_Queue";
+            // Send request XMLmessage
+            AMQP.BasicProperties basicProperties = new AMQP.BasicProperties()
+                    .builder()
+                    .contentType("text/plain")
+                    .deliveryMode(1)
+                    .replyTo(replyKey)
+                    .build();
+            replyChannel.basicPublish("", replyKey, null, message.getBytes());
+
+            System.out.println("Message is send to queue: " + replyKey);
+            replyChannel.close();
+            connection.close();
+
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
     }
 
 }
